b2a221812c4d4aed92c134f23c18b08283b8ecf5
Alejandro Calvo <offztinger@gmail.com>
2024-02-22 18:39:13 -0500

    Subiendo repositorio SpringBoot

---
+++ src/main/java/co/edu/elpoli/task/TaskApplication.java
@@ -0,0 +1,13 @@
+package co.edu.elpoli.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskApplication.class, args);
+	}
+
+}
