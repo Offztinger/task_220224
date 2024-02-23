730e0152ba5966090b9c1687298b07e1b885862f
Alejandro Calvo <offztinger@gmail.com>
2024-02-22 19:07:34 -0500

    Subiendo cambios

--- src/main/java/co/edu/elpoli/task/persistence/entity/Task.java
+++ src/main/java/co/edu/elpoli/task/persistence/entity/Task.java
@@ -1,16 +1,19 @@
 package co.edu.elpoli.task.persistence.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "tasks")
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="task_id")
     private long taskId;
     private String name;
     private String description;
+    @Column(name="create_date")
     private LocalDate dataCreate;
     private boolean finished;
     private TaskStatus status;
